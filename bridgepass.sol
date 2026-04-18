39f271a99c89882c75ccd8eaef5cc560b439187f
Yevheniia Muravska <zeniuha13@gmail.com>
2026-04-18 20:28:57 +0200

    Update bridgepass.sol

--- bridgepass.sol
+++ bridgepass.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT
+//SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
@@ -6,18 +6,17 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 contract BridgePass is ERC721 {
     uint256 private _nextTokenId;
     
-    // Zmienna śledząca, kto już odebrał bilet
+    //who has a ticket
     mapping(address => bool) public hasMinted;
 
     constructor() ERC721("Bridge Phygital Pass", "BPP") {}
 
     function mint() public {
-        // ZABEZPIECZENIE: Sprawdza, czy ten portfel już czegoś nie wybił
-        require(!hasMinted[msg.sender], "Zabezpieczenie: Posiadasz juz bilet na tym portfelu!");
+        //security patch
+        require(!hasMinted[msg.sender], "You already have a ticket in this wallet");
         
         uint256 tokenId = _nextTokenId++;
         
-        // Zaznaczamy, że ten użytkownik właśnie odebrał bilet
         hasMinted[msg.sender] = true;
         
         _safeMint(msg.sender, tokenId);
